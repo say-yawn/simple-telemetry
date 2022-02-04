c6a5ea66a479cf8122829a1512c342e2ab0f8e98
Se Yeon Kim <sykim@mozilla.com>
2022-02-03 21:31:52 -0600

    Routing the project to use api endpoints

--- telemetry/urls.py
+++ telemetry/urls.py
@@ -5,6 +5,8 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+# Register new endpoints
+router.register(r'alias-created', views.AliasCreatedViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
