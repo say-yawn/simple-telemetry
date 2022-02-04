20a21e893a489784d421d7e9ee9fcb8119bcd64f
Se Yeon Kim <sykim@mozilla.com>
2022-02-03 21:23:09 -0600

    Add API views and serializers for events

---
+++ api/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import (
+    UserSerializer,
+    GroupSerializer,
+    AliasCreatedSerializer,
+    AddonInteractionSerializer,
+)
+from event.models import (
+    AliasCreated,
+    AddonInteraction,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AliasCreatedViewSet(viewsets.ModelViewSet):
+    queryset = AliasCreated.objects.all()
+    serializer_class = AliasCreatedSerializer
+
+
+# create a viewset for the AddonOnbaording here
+    
